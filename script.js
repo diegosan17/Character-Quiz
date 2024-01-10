f29429c76451194f777c68d2753075f2a75eebf5
diegosan17 <144826622+diegosan17@users.noreply.github.com>
2024-01-10 00:37:04 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 var answer1;
 var answer2;
-var counter = 0;
-
-$("#result").click(function() {
-    answer1 = $('.question1').val();
-    console.log(answer1);
-    answer2 = $('.question2').val();
-    console.log(answer2);
-    if (answer1 === "yes" && answer2 > 5) {
-        $("#dipper").css("display", "block");
-        console.log("dipper");
-    } else if (answer2 < 6 && answer1 === "yes") {
-        $("#mabel").show();
-        console.log("mabel");
-    } else if (answer1 === "no" && answer2 > 5) {
-        $("#bill").show();
-        console.log("bill");
-    } else if (answer2 < 6 && answer1 === "no") {
-        $("#stan").show();
-        console.log("stan");
-    } else {
-        alert("Enter Correct Values");
-    }
+$(document).ready(function() {
+    let quizCount = 0;
+    $("#results").click(function() {
+        answer1 = $('.question1').val();
+        console.log(answer1);
+        answer2 = $('.question2').val();
+        console.log(answer2);
+        if (answer1 === "yes" && answer2 > 5) {
+            $("#dipper").css("display", "block");
+            $("#name").text("Dipper");
+            console.log("dipper");
+        } else if (answer2 < 6 && answer1 === "yes") {
+            $("#mabel").show();
+            $("#name").text("Mabel");
+            console.log("mabel");
+        } else if (answer1 === "no" && answer2 > 5) {
+            $("#bill").show();
+            $("#name").text("Bill Cipher");
+            console.log("bill");
+        } else if (answer2 < 6 && answer1 === "no") {
+            $("#stan").show();
+            $("#name").text("Grunkle Stan");
+            console.log("stan");
+        } else {
+            alert("Enter Correct Values");
+        }
+        quizCount++;
+        updateQuizCount();
+    
+        function updateQuizCount() {
+            const quizCountText = `Quiz has been taken ${quizCount} times.`;
+            $('#counter').append(`<p>${quizCountText}</p>`);}   
+    });
 });
-$("#result").click(function() {
-    counter += 1;
-    $("#coun").text("Number of Attempts: " + counter);
+$('#restartBtn').click(function() {
+    $(location).attr('href', );
+    $('.question1').val('');
+    $('.question2').val('');
+    $('#counter').empty();
+    $("#mabel").hide();
+    $("#dipper").hide();
+    $("#stan").hide();
+    $("#bill").hide();
+    $("#name").text("");
 });
